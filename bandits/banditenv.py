97479d7bf929b9c7e1e354db81f45ea04b077f79
Abhinav-0103 <abhinava808@gmail.com>
2025-05-28 21:38:53 +0530

    Updated

--- bandits/banditenv.py
+++ bandits/banditenv.py
@@ -1,11 +1,11 @@
 import numpy as np
 
-class Bandit :
-    def __init__(self, k: int=10) :
+class BanditEnv :
+    def __init__(self, k: int = 10) :
         self.k = k
-        self.q_star = np.random.normal(loc=0.0, scale=1.0, size=k)
+        self.q_star = np.random.normal(loc=0.0, scale=1.0, size=self.k)
         self.optimal_action = np.argmax(self.q_star)
     
-    def step(self, action: int) -> float :
-        true_value = self.q_star[action]
-        return np.random.normal(loc=true_value, scale=1.0)
+    def step(self, action: int) :
+        q_star = self.q_star[action]
+        return np.random.normal(loc=q_star, scale=1.0)
